87e1f0221b78187d4a59990745578e1e1bd53810
joshua <joshua@CorretjerPC.lan>
2020-05-18 23:47:17 -0700

    done

--- src/Game/Zelda/Entities/Statics/MMMovingPad.java
+++ src/Game/Zelda/Entities/Statics/MMMovingPad.java
@@ -3,6 +3,8 @@ package Game.Zelda.Entities.Statics;
 import Game.GameStates.Zelda.ZeldaMMGameState;
 import Game.PacMan.World.MapBuilder;
 import Game.Zelda.Entities.MMBaseEntity;
+import Game.Zelda.Entities.Dynamic.Direction;
+import Game.Zelda.World.Map;
 import Main.Handler;
 import Resources.Images;
 
@@ -23,29 +25,44 @@ public class MMMovingPad extends MMBaseEntity {
         this.width = MapBuilder.pixelMultiplier;
         this.height = MapBuilder.pixelMultiplier;
         bounds = new Rectangle(x ,y ,MapBuilder.pixelMultiplier,MapBuilder.pixelMultiplier);
+        Map map = new Map(handler);
 
     }
 
     @Override
     public void tick() {
-        if(handler.getState() instanceof ZeldaMMGameState && ((ZeldaMMGameState)handler.getState()).map.link.interactBounds.intersects(bounds) && handler.getKeyManager().keyJustPressed(KeyEvent.VK_P)){
+        if(handler.getState() instanceof ZeldaMMGameState && handler.getKeyManager().shift){
 //            ((ZeldaMMGameState)handler.getState()).map.link.x+=width;
 //            ((ZeldaMMGameState)handler.getState()).map.link.y = linkedY;
-        	
-        	mov=true;
-        }
-        if (mov) {
-        	recursiveMove(((ZeldaMMGameState)handler.getState()).map.link.x,((ZeldaMMGameState)handler.getState()).map.link.y);
+    	if(sprite==Images.movingTiles.get(0)) {
+    		((ZeldaMMGameState)handler.getState()).map.link.x+=5;
+		}else if(sprite==Images.movingTiles.get(1)){
+			((ZeldaMMGameState)handler.getState()).map.link.y+=5;
+		}else if(sprite==Images.movingTiles.get(2)) {
+			((ZeldaMMGameState)handler.getState()).map.link.x-=5;
+		}else if(sprite==Images.movingTiles.get(3)) {
+			((ZeldaMMGameState)handler.getState()).map.link.y-=5;
+		}
+//        	movingStep();
         }
         
     }
     
-    public void recursiveMove(int x, int y) {
-    	if(((ZeldaMMGameState)handler.getState()).map.link.interactBounds.intersects(bounds) && sprite==Images.movingTiles.get(0)) {
-    		recursiveMove(x+(width+10),y);
-    	}else {
-    		mov=false;
-//    		((ZeldaMMGameState)handler.getState()).map.link.move();
+    public void movingStep() {
+    	Map map = new Map(handler);
+    	for(MMBaseEntity tileR: map.getBlocksOnMap()) {
+    		if(tileR instanceof MMMovingPad && map.link.interactBounds.intersects(tileR.bounds)) {
+    			if(tileR.sprite.equals(Images.movingTiles.get(0))) {
+    				map.link.move(Direction.RIGHT);
+    			}else if(tileR.sprite.equals(Images.movingTiles.get(1))) {
+    				map.link.move(Direction.DOWN);
+    			}else if(tileR.sprite.equals(Images.movingTiles.get(2))) {
+    				map.link.move(Direction.LEFT);
+    			}else if(tileR.sprite.equals(Images.movingTiles.get(3))) {
+    				map.link.move(Direction.UP);
+    			}
+    			movingStep();
+    		}
     	}
     }
 
