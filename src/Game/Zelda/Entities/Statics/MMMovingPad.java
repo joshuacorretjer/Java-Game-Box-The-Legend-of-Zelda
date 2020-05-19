495ddd77f568af84a040533d807ccf432a5eff1b
joshua <joshua@CorretjerPC.lan>
2020-05-18 19:07:42 -0700

    aaa

--- src/Game/Zelda/Entities/Statics/MMMovingPad.java
+++ src/Game/Zelda/Entities/Statics/MMMovingPad.java
@@ -27,15 +27,14 @@ public class MMMovingPad extends MMBaseEntity {
 
     @Override
     public void tick() {
-        if (handler.getState() instanceof ZeldaMMGameState && ((ZeldaMMGameState)handler.getState()).map.link.interactBounds.intersects(bounds) && handler.getKeyManager().keyJustPressed(KeyEvent.VK_P)){
-//            ((ZeldaMMGameState)handler.getState()).map.link.x = linkedX;
+        if(handler.getState() instanceof ZeldaMMGameState && ((ZeldaMMGameState)handler.getState()).map.link.interactBounds.intersects(bounds) && handler.getKeyManager().keyJustPressed(KeyEvent.VK_P)){
+//            ((ZeldaMMGameState)handler.getState()).map.link.x+=width;
 //            ((ZeldaMMGameState)handler.getState()).map.link.y = linkedY;
         	
         	mov=true;
         }
         if (mov) {
         	recursiveMove(((ZeldaMMGameState)handler.getState()).map.link.x,((ZeldaMMGameState)handler.getState()).map.link.y);
-        	mov = false;
         }
         
     }
@@ -43,9 +42,9 @@ public class MMMovingPad extends MMBaseEntity {
     public void recursiveMove(int x, int y) {
     	if(((ZeldaMMGameState)handler.getState()).map.link.interactBounds.intersects(bounds) && sprite==Images.movingTiles.get(0)) {
     		recursiveMove(x+(width+10),y);
-    		
     	}else {
     		mov=false;
+    		((ZeldaMMGameState)handler.getState()).map.link.move();
     	}
     }
 
